48f11f5334697001265c215cbc2f251656a8fa2f
vince93290 <gameiro93290@gmail.com>
2020-12-22 11:18:35 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: "hellosign-paramedic",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
